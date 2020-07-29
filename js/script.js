d29506e2951d2a293048722b70bf5d03d6d66bd1
Mateusz Krupa <mateuszandrzejkrupa@gmail.com>
2020-07-29 07:33:00 +0200

    Display arcticles after click on title by js.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 'use strict';
 {
-/*document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });
-  */
+
+/* [DONE] Click listener on lef column */
 
 const titleClickHandler = function(){
     event.preventDefault(); //ok-blokada przewijania strony do #
@@ -38,7 +35,7 @@ const titleClickHandler = function(){
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-  const article = document.querySelector(articleHref); //tego nie kumam
+  const article = document.querySelector(articleHref); //TEGO NIE KUMAM
   console.log(article)
 
   /* [DONE] add class 'active' to the correct article */
@@ -47,12 +44,8 @@ const titleClickHandler = function(){
 
 }
   
-const links = document.querySelectorAll('.titles a');
-for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-}
+/* [DONE] genertor links - left column */
 
-// [FROM KODILLA]
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
@@ -61,15 +54,50 @@ function generateTitleLinks(){
 //  console.log('leftLinksGenerator-tested');
 
   /* [DONE] clear left panel - remove links list constent */
-  
+
   const titleLinks = document.querySelector('.titles');
-  titleLinks.innerHTML = '';
+  //titleLinks.innerHTML = '';
+  let html = '';
 
-  /* [IN PROCESS] for all articles - get a id of article and save as a const */
+  /* [DONE] for all articles: */ 
 
-  /* [QUERRY] for all articles - find 'title' of artile save as a const */
-  /* [QUERRY] for all articles - generate HTML code and save as a const */
-  /* [QUERRY] for all articles - display HTML code in a left column */
+  const articles = document.querySelectorAll('.post');
+  //console.log(articles);
+
+  for (let article of articles){
+
+    /* [DONE] get a id of article and save as a const */
+
+    const articleId = article.getAttribute('Id');
+    //console.log('Id: ', articleId);
+  
+    /* [DONE] - find 'title' of article save as a const */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;//dlaczego innerHTMLmoze byc puste ?> ();
+    //console.log('tytuł artykułu: ', articleTitle);
+    
+    /* [DONE] generate HTML code and save as a const */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    //console.log('HTML code: ', linkHTML);
+    
+    /* [DONE] display links in a  column */
+    
+    //[v1] titleLinks.innerHTML = titleLinks.innerHTML + linkHTML;
+    //[v2] titleLinks.insertAdjacentHTML("beforeend", linkHTML);
+    //[v3]
+    html = html + linkHTML;
+    console.log('htmllink', html);
+   }
+   titleLinks.innerHTML = html;
+  
+  /* Initiate titleClickHandler */
+
+  const links = document.querySelectorAll('.titles a');
+  console.log('to sa linki', links)
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    }
   }
 
 generateTitleLinks();
